9da2ecb7594bd6b1b094e4fb418dc064a5c06a62
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-07-28 15:56:04 +0900

    feat: 관리자용 기능 구현(승인, 반려, 지급)

---
+++ backend_set/src/main/java/org/funding/userLoan/dto/CancelLoanRequestDTO.java
@@ -0,0 +1,8 @@
+package org.funding.userLoan.dto;
+
+import lombok.Data;
+
+@Data
+public class CancelLoanRequestDTO {
+    private Long userId;
+}
---
+++ backend_set/src/main/java/org/funding/userLoan/dto/UserLoanResponseDTO.java
@@ -0,0 +1,9 @@
+package org.funding.userLoan.dto;
+
+import lombok.Data;
+
+@Data
+public class UserLoanResponseDTO {
+    private String userName;
+    private Integer loanAmount;
+}
