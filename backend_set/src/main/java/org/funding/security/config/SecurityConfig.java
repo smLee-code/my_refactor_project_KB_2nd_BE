72cd0faaad5eeb35d2bde1045d5f933072d21d9a
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-08-07 15:21:38 +0900

    수정

--- backend_set/src/main/java/org/funding/security/config/SecurityConfig.java
+++ backend_set/src/main/java/org/funding/security/config/SecurityConfig.java
@@ -143,7 +143,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             "/api/project/related/{id}",
                             "/api/project/top",
                             "/api/project/trend",
-                            "/api/project/distribution/type"
+                            "/api/project/distribution/type",
                             "/api/mail/**"
 
                     ).permitAll()
