a7b932c1887dc98d28ec52c027ab87868f50f32a
Sang-Myeong Lee <teamjuji5@naver.com>
2025-08-10 20:56:05 +0900

    #214 refactor: getChatHistory 리팩토링 (DTO 내용 변경)

--- backend_set/src/main/java/org/funding/chatting/service/ChattingService.java
+++ backend_set/src/main/java/org/funding/chatting/service/ChattingService.java
@@ -4,15 +4,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.funding.chatting.dao.ChattingDAO;
 import org.funding.chatting.dto.ChattingMessage;
+import org.funding.chatting.dto.ChattingMessageResponseDTO;
+import org.funding.chatting.vo.ChattingMessageVO;
+import org.funding.user.dao.MemberDAO;
+import org.funding.user.vo.MemberVO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ChattingService {
 
+    private final MemberDAO memberDAO;
     private final ChattingDAO chattingDAO;
 
     public void saveMessage(ChattingMessage message) {
@@ -21,7 +27,22 @@ public class ChattingService {
 
     }
 
-    public List<ChattingMessage> getMessages(Long projectId) {
-        return chattingDAO.findMessagesByRoomId(projectId);
+    public List<ChattingMessageResponseDTO> getMessages(Long projectId, Long userId) {
+        List<ChattingMessageVO> chattingList = chattingDAO.findMessagesByRoomId(projectId);
+
+        return chattingList.stream()
+                .map(chattingVO -> {
+
+                    MemberVO memberVO = memberDAO.findById(chattingVO.getUserId());
+
+                    return ChattingMessageResponseDTO.builder()
+                            .sender(memberVO.getUsername())
+                            .isSelf(Objects.equals(userId, chattingVO.getUserId()))
+                            .content(chattingVO.getContent())
+                            .timestamp(chattingVO.getTimestamp())
+                            .build();
+                })
+                .toList();
+
     }
 }
