6864e0f8671e05fb0408b9643a1c9fd91d929e3a
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-08-19 09:41:46 +0900

    fix: ai 이미지 검증 수정

--- backend_set/src/main/java/org/funding/userLoan/vo/UserLoanVO.java
+++ backend_set/src/main/java/org/funding/userLoan/vo/UserLoanVO.java
@@ -3,6 +3,8 @@ package org.funding.userLoan.vo;
 import lombok.Data;
 import org.funding.userLoan.vo.enumType.SuccessType;
 
+import java.time.LocalDateTime;
+
 @Data
 public class UserLoanVO {
     private Long userLoanId;
@@ -11,4 +13,5 @@ public class UserLoanVO {
     private Integer loanAmount; // 대출 금액
     private SuccessType loanAccess; // 대출 승인 여부
     private boolean fullPayment; // 완납 여부
+    private LocalDateTime createAt; // 생성 시간
 }
